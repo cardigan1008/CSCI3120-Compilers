3ecb5521580a2fc49092a89cc5277043859b1b4f
cardigan1008 <ybni@cse.cuhk.edu.hk>
2025-09-07 22:58:40 +0800

    fix: revert test

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,5 @@ public class Main {
         if (args.length == 0) {
             System.exit(1);
         }
-        String inputFile = args[0];
-        try {
-            java.nio.file.Path path = java.nio.file.Paths.get(inputFile);
-            java.util.List<String> lines = java.nio.file.Files.readAllLines(path);
-            for (String line : lines) {
-                System.out.println(line);
-            }
-        } catch (java.io.IOException e) {
-            System.exit(1);
-        }
     }
 }
