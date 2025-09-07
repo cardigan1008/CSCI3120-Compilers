ed591f4935556056599cceb57a136eeecdaa1e67
tracer-compilers2025 <compilers@cse.cuhk.edu.hk>
2025-09-07 19:53:34 +0800

    > make 1155250517 root Linux iZj6c6lcbkkvodzwhuo9hbZ 6.8.0-64-generic #67-Ubuntu SMP PREEMPT_DYNAMIC Sun Jun 15 20:23:31 UTC 2025 x86_64 x86_64 x86_64 GNU/Linux  19:53:34 up  3:33,  2 users,  load average: 0.24, 0.17, 0.16 e951d5708b4a30ad373a156e2f4b8fb0bd091b7e

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,15 @@ public class Main {
         if (args.length == 0) {
             System.exit(1);
         }
+        String inputFile = args[0];
+        try {
+            java.nio.file.Path path = java.nio.file.Paths.get(inputFile);
+            java.util.List<String> lines = java.nio.file.Files.readAllLines(path);
+            for (String line : lines) {
+                System.out.println(line);
+            }
+        } catch (java.io.IOException e) {
+            System.exit(1);
+        }
     }
 }
